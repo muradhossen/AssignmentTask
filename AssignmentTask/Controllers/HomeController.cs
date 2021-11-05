441e9d1a2f86dbc00c12adea3d3a90487b06d0c9
muradhossen <hossenmurad46@gmail.com>
2021-11-05 12:51:53 +0600

    Complete Create and Delete

--- AssignmentTask/Controllers/HomeController.cs
+++ AssignmentTask/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace AssignmentTask.Controllers
                               Total = (double)o.Total,
                               Paid = o.Paid,
                               Due = (double)o.Total - o.Paid
-                          }).ToList(); 
+                          }).ToList();
             return View(result);
         }
 
@@ -48,60 +48,132 @@ namespace AssignmentTask.Controllers
         }
         public JsonResult SaveOrder(string gTotal, string paid, User[] user, OrderItemViewModel[] order)
         {
-            try
+            if(gTotal!="" && paid!="" && user!=null && order != null)
             {
-
-                List<OrderItemViewModel> Orders = order.ToList();
-                //Customer Table
-                User vUser = new User()
+                try
                 {
-                    name = user[0].name,
-                    address = user[0].address,
-                    phone = user[0].phone
-                };
 
-                Customer mCustomer = new Customer()
-                {
-                    Name = vUser.name,
-                    Address = vUser.address,
-                    Phone = vUser.phone
-                };
-                db.Customers.Add(mCustomer);
-                db.SaveChanges();
-                var cId = db.Customers.OrderByDescending(x => x.Id).FirstOrDefault().Id;
-
-                //Order Table
-                Order mOrder = new Order
-                {
-                    CustomerId = cId,
-                    Paid = Convert.ToInt32(paid),
-                    Total = Convert.ToInt32(gTotal)
-                };
-                db.Orders.Add(mOrder);
-                db.SaveChanges();
-                var oId = db.Orders.OrderByDescending(x => x.Id).FirstOrDefault().Id;
-
-                //Order Item Table
-                foreach (var item in Orders)
-                {
-                    OrderItem orderItem = new OrderItem
+                    List<OrderItemViewModel> Orders = order.ToList();
+                    //Customer Table
+                    User vUser = new User()
                     {
-                        ProductId = item.itemId,
-                        OrderId = oId,
-                        Quantity = item.quantity,
+                        name = user[0].name,
+                        address = user[0].address,
+                        phone = user[0].phone
                     };
-                    db.OrderItems.Add(orderItem);
+
+                    Customer mCustomer = new Customer()
+                    {
+                        Name = vUser.name,
+                        Address = vUser.address,
+                        Phone = vUser.phone
+                    };
+                    db.Customers.Add(mCustomer);
+                    db.SaveChanges();
+                    var cId = db.Customers.OrderByDescending(x => x.Id).FirstOrDefault().Id;
+
+                    //Order Table
+                    Order mOrder = new Order
+                    {
+                        CustomerId = cId,
+                        Paid = Convert.ToInt32(paid),
+                        Total = Convert.ToInt32(gTotal)
+                    };
+                    db.Orders.Add(mOrder);
+                    db.SaveChanges();
+                    var oId = db.Orders.OrderByDescending(x => x.Id).FirstOrDefault().Id;
+
+                    //Order Item Table
+                    foreach (var item in Orders)
+                    {
+                        OrderItem orderItem = new OrderItem
+                        {
+                            ProductId = item.itemId,
+                            OrderId = oId,
+                            Quantity = item.quantity,
+                        };
+                        db.OrderItems.Add(orderItem);
+                    }
+                    db.SaveChanges();
                 }
-                db.SaveChanges();
-            }
-            catch (Exception e)
-            {
+                catch (Exception e)
+                {
 
-                throw;
+                    throw;
+                }
             }
-
+           
+            else return Json("Please Fill Up All Properties", JsonRequestBehavior.AllowGet);
 
             return Json("Success", JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult viewDeails(int id)
+        {
+            int intOId = Convert.ToInt32(id);
+
+            var customer = (from o in db.Orders
+                            join cus in db.Customers on o.CustomerId equals cus.Id
+
+                            select new SalesViewModel
+                            {
+                                OrderId = o.Id,
+                                Name = cus.Name,
+                                Phone = cus.Phone,
+                                Address = cus.Address,
+                                Total = (double)o.Total,
+                                Paid = o.Paid,
+                                Due = (double)o.Total - o.Paid
+                            }).Where(x => x.OrderId == intOId).ToList();
+
+            var orderItems = (from o in db.Orders
+                              join oItem in db.OrderItems on o.Id equals oItem.OrderId
+                              join pro in db.Products on oItem.ProductId equals pro.Id
+                              where o.Id == intOId
+                              select new
+                              {
+                                  OrderId = o.Id,
+                                  ProductId = pro.Id,
+                                  ProductName = pro.Name,
+                                  Quantity = oItem.Quantity,
+                                  Price = pro.Price
+                              }).ToList();
+            DetailsViewModel vModel = new DetailsViewModel();
+
+            vModel.OrderId = customer.FirstOrDefault().OrderId;
+            vModel.Name = customer.FirstOrDefault().Name;
+            vModel.Address = customer.FirstOrDefault().Address;
+            vModel.Phone = customer.FirstOrDefault().Phone;
+            vModel.Total = customer.FirstOrDefault().Total;
+            vModel.Paid = customer.FirstOrDefault().Paid;
+            vModel.Due = customer.FirstOrDefault().Total - customer.FirstOrDefault().Paid;
+
+            foreach (var item in orderItems)
+            {
+                DetailsItemViewModel itemViewModel = new DetailsItemViewModel();
+                itemViewModel.ItemId = item.ProductId;
+                itemViewModel.ItemName = item.ProductName;
+                itemViewModel.Price = item.Price;
+                itemViewModel.Quantity = item.Quantity;
+                itemViewModel.Total = item.Price * item.Quantity;
+                vModel.OrderItemList.Add(itemViewModel);
+            }
+
+            return View(vModel);
+        }
+
+        public ActionResult DeleteSales(int id)
+        {
+            var orderItems= db.OrderItems.Where(x => x.OrderId == id).ToList();
+            foreach (var item in orderItems)
+            {
+                db.OrderItems.Remove(item);
+            }
+            var order= db.Orders.Find(id);
+            if (order != null) db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ AssignmentTask/ViewModel/DetailsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssignmentTask.ViewModel
+{
+    public class DetailsViewModel
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public double Total { get; set; }
+        public int? Paid { get; set; }
+        public double? Due { get; set; }
+        public List<DetailsItemViewModel> OrderItemList = new List<DetailsItemViewModel>();
+    }
+    public class DetailsItemViewModel
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public double? Price { get; set; }
+        public int Quantity { get; set; }
+        public double? Total { get; set; }
+    }
+}
